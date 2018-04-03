8f4d67772d3246e9508f9532eb1066dd5cb3aeac
SyaifulAndiIrawan <32381985+syaifulandiirawan@users.noreply.github.com>
2018-04-03 10:35:06 +0800

    Instance Object

--- src/tugaspraktek02/RectangleAksi.java
+++ src/tugaspraktek02/RectangleAksi.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugaspraktek02;
 
-/**
- *
- * @author udin'
- */
 public class RectangleAksi {
-    
+  public static void main(String[] args){   
+          Rectangle r1 = new Rectangle();  
+  }
+  
+  
 }
